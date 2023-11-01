9050cb03157f520c16980817a1b64c1b2113cc11
P1su <pissssssu@naver.com>
2023-11-02 00:40:39 +0900

    navigate event

--- app/src/main/java/com/example/androidproject/MainActivity.kt
+++ app/src/main/java/com/example/androidproject/MainActivity.kt
@@ -2,11 +2,25 @@ package com.example.androidproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.androidproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val navController = binding.frgNav.getFragment<NavHostFragment>().navController
+
+        binding.btmNav.setupWithNavController(navController)
+
+        setContentView(binding.root)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = binding.frgNav.getFragment<NavHostFragment>().navController
+        return super.onSupportNavigateUp()
     }
 }
